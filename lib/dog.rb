580c3d8b1bec212bf2dc391e64c7fce22802eff1
Kierra M <k.merriman32@hotmail.com>
2019-08-31 13:29:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,14 @@
 class Dog 
   
-  def name=(dog_name)
+  def .name=(dog_name)
     @this_dogs_name = name
   end
   
-  def name 
+  def .name 
     @this_dogs_name
   end
   
-  def bark
+  def .bark
     puts "woof!"
   end 
   
